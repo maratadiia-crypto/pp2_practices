6b76a3266a851eeab0166dc5773da39ed3001a92
adiyusha <maratadiia@gmail.com>
2026-04-17 14:39:31 +0500

    Add Mickey Clock project

--- practice9/mickeys_clock/clock.py
+++ practice9/mickeys_clock/clock.py
@@ -1,54 +1,61 @@
 import pygame
-from datetime import datetime
+import time
+import math
 
-class MickeyClock:
-    def __init__(self, clock_img, left_arm_img, right_arm_img):
-        full_clock = pygame.image.load(clock_img).convert()
-        self.clock = pygame.Surface((720, 720)).convert()
-        self.clock.blit(full_clock, (0, 0), (0, 280, 720, 720))
-        self.left_arm = pygame.image.load(left_arm_img).convert_alpha()
-        self.right_arm = pygame.image.load(right_arm_img).convert_alpha()
-        self.left_arm.set_colorkey((255, 255, 255))
-        self.right_arm.set_colorkey((255, 255, 255))
-        self.left_arm = pygame.transform.scale(self.left_arm, (200, 260))
-        self.right_arm = pygame.transform.scale(self.right_arm, (230, 300))
-        self.center = (360, 430)
-        self.left_pivot = (180, 200)
-        self.right_pivot = (150, 250)
-    def blit_rotate(self, screen, image, pivot, angle, pos):
-        rect = image.get_rect(
-            topleft=(pos[0] - pivot[0], pos[1] - pivot[1])
-        )
-        offset = pygame.math.Vector2(pos) - rect.center
-        rotated_offset = offset.rotate(-angle)
-        rotated_center = (
-            pos[0] - rotated_offset.x,
-            pos[1] - rotated_offset.y
-        )
-        rotated_image = pygame.transform.rotate(image, angle)
-        rotated_rect = rotated_image.get_rect(
-            center=rotated_center
-        )
+pygame.init()
 
-        screen.blit(rotated_image, rotated_rect)
-    def draw(self, screen):
-        screen.blit(self.clock, (0, 0))
-        now = datetime.now()
-        seconds = now.second
-        minutes = now.minute
-        second_angle = -(seconds * 6)
-        minute_angle = -(minutes * 6)
-        self.blit_rotate(
-            screen,
-            self.left_arm,
-            self.left_pivot,
-            second_angle,
-            self.center
-        )
-        self.blit_rotate(
-            screen,
-            self.right_arm,
-            self.right_pivot,
-            minute_angle,
-            self.center
-        )
+WIDTH, HEIGHT = 900, 835
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Clock")
+
+
+WHITE = (255, 255, 255)
+
+left = pygame.image.load('practice9/mickeys_clock/images/leftarm.png')
+right = pygame.image.load('practice9/mickeys_clock/images/rightarm.png')
+background = pygame.image.load('practice9/mickeys_clock/images/mickeyclock.jpeg')
+
+bg_rect = background.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+
+CENTER_X, CENTER_Y = WIDTH // 2, HEIGHT // 2 + 50
+
+OFFSET_Y = -40
+
+left_rect = left.get_rect()
+right_rect = right.get_rect()
+
+left_rect = left.get_rect(center=(CENTER_X, CENTER_Y + OFFSET_Y))
+right_rect = right.get_rect(center=(CENTER_X, CENTER_Y + OFFSET_Y))
+
+def rotate_and_blit(image, angle, pivot):
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_rect = rotated_image.get_rect(center=pivot)
+    screen.blit(rotated_image, rotated_rect.topleft)
+
+    
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+            running = False
+
+    current_time = time.localtime()
+    seconds = current_time.tm_sec
+    minutes = current_time.tm_min
+
+    second_angle = (-seconds * 6) + 90 
+    minute_angle = (-minutes * 6) + 90
+
+   
+    screen.fill(WHITE)
+
+    screen.blit(background, bg_rect)
+
+    rotate_and_blit(left, second_angle, (CENTER_X, CENTER_Y))  
+    rotate_and_blit(right, minute_angle, (CENTER_X, CENTER_Y)) 
+
+    pygame.display.flip()
+    pygame.time.delay(100)
+
+pygame.quit()
